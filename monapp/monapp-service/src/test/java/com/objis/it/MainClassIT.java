a8a3adbfa4a299764bcbe78ccc8b5bbcae797140
gsourdrille <gsourdrille@gmail.com>
2014-01-23 10:07:36 +0100

    Ajout de IT

---
+++ monapp/monapp-service/src/test/java/com/objis/it/MainClassIT.java
@@ -0,0 +1,14 @@
+package com.objis.it;
+
+import junit.framework.Assert;
+
+import org.junit.Test;
+
+public class MainClassIT {
+	
+	@Test
+	public void testMainClass(){
+		Assert.assertTrue(true);
+	}
+
+}
---
+++ monapp/monapp-service/src/test/java/com/objis/test/MainClassTest.java
@@ -0,0 +1,14 @@
+package com.objis.test;
+
+import junit.framework.Assert;
+
+import org.junit.Test;
+
+public class MainClassTest {
+	
+	@Test
+	public void testMainClass(){
+		Assert.assertTrue(true);
+	}
+
+}
